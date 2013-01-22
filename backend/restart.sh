8dd27a0299673bd148dc2417e080e0994b29d2b8
Indz <florian@klockenkemper.com>
2013-01-22 12:03:45 +0100

    remove 5.2 from backend

--- backend/restart.sh
+++ backend/restart.sh
@@ -3,4 +3,3 @@ killall twistd
 # twistd -y server-4.1.tac --pidfile=tmp/server-4.1.pid --logfile log/server-4.1.log
 # twistd -y server-4.2.tac --pidfile=tmp/server-4.2.pid --logfile log/server-4.2.log
 twistd -y server-5.0.tac --pidfile=tmp/server-5.0.pid --logfile log/server-5.0.log
-twistd -y server-5.2.tac --pidfile=tmp/server-5.2.pid --logfile log/server-5.2.log
