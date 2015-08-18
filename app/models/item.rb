2501dc892816e98ae46a758fdbeee285e55955a3
Tim Wojtulewicz <timwoj@ieee.org>
2015-08-18 03:59:00 +0000

    Switch over to whitelist bonus IDs instead of blacklisting.  Also fix trade-skill items to load all of the stages.

--- app/models/item.rb
+++ app/models/item.rb
@@ -224,35 +224,27 @@ class Item
     end
   end
 
-  BLACKLIST_EXTRA_SOCKETS = [3, 497, 523, 563, 564, 565, 572] # extra sockets
+  # these are the only bonus IDs we care about displaying on the gear popouts.  They're
+  # mostly just the different difficulty levels that can be on gear.  Anything not listed
+  # here is ignored and a separate item is not created in the database for it.  This is
+  # how we prevent duplicates of items from showing up on the UI.  There are so many bonus
+  # IDs that it's easier to whitelist the ones we want, instead of blacklisting the ones
+  # we don't.
+  # 
+  # This whitelist skips any of the randomly generated bonus IDs such as any "of the"
+  # bonuses and any "100%" IDs.  It also skips any bonus IDs that are sockets.
+  # TODO: make this autogenerate from the CASC game data
+  # TODO: add ring bonus IDs 622-641 separately in a loop
+  BONUS_ID_WHITELIST = [1, 15, 17, 18, 44, 171, 448, 449, 450, 451, 499, 526, 527, 545, 546, 547,
+                        553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 566, 567, 571, 573, 575,
+                        576, 577, 582, 583, 591, 592, 593, 594, 602, 609, 615, 617, 618, 619, 620,
+                        642, 644, 645, 646, 648, 651, 656, 692]
 
-  BLACKLIST_RANDOM_SUFFIXES = [3, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 45,
-                           46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68,
-                           69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91,
-                           92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
-                           112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129,
-                           130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147,
-                           148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165,
-                           166, 167, 168, 169, 170, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187,
-                           188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205,
-                           206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
-                           224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241,
-                           242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259,
-                           260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277,
-                           278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295,
-                           296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313,
-                           314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331,
-                           332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349,
-                           350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367,
-                           368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 379, 380, 381, 382, 383, 384, 385,
-                           386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399, 400, 401, 402, 403,
-                           404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421,
-                           422, 423, 424, 425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436, 437, 438, 439,
-                           440, 441, 442, 443, 444, 445, 446, 447, 459, 460, 461, 462, 463, 464, 465, 466, 467, 468,
-                           469, 470, 471, 472, 473, 474, 475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486,
-                           487, 488, 489, 490, 491, 492, 497]
-
-  BLACKLIST_TERTIARY_STATS = [40, 41, 42, 43]
+  # For some reason the crafted items don't come with the "stage" bonus IDs in their
+  # chanceBonusList entry.  This is the list of bonus IDs for those stages and is
+  # handled slightly differently.  See below for the check for trade-skill for more
+  # details.
+  TRADESKILL_BONUS_IDS = [525, 558, 559, 594, 619, 620]
 
   def self.wod_import(id, context = '', bonuses = nil)
     source = 'wowapi'
@@ -272,9 +264,7 @@ class Item
       # if bonus is in blacklist filter out
       return if bonuses.nil?
       bonuses.clone.each do |bonus|
-        bonuses.delete(bonus) if BLACKLIST_EXTRA_SOCKETS.include? bonus # extra sockets
-        bonuses.delete(bonus) if BLACKLIST_TERTIARY_STATS.include? bonus # avoidance, leech, speed, indestructible
-        bonuses.delete(bonus) if BLACKLIST_RANDOM_SUFFIXES.include? bonus # random suffixes
+        bonuses.delete(bonus) unless BONUS_ID_WHITELIST.include? bonus
       end
       return if bonuses.empty?
       # make a special import with the given and possible missing bonus id data from armory
@@ -290,6 +280,7 @@ class Item
       puts e.message
       return
     end
+
     json_data['availableContexts'].each do |context|
       if context == ''
         context_data = json_data
@@ -297,13 +288,20 @@ class Item
         context_data = WowArmory::Document.fetch 'us', '/wow/item/%d/%s' % [id, context], {}, :json
       end
       context_data['bonusSummary']['defaultBonusLists'].clone.each do |defaultBonusListsId|
-        context_data['bonusSummary']['defaultBonusLists'].delete(defaultBonusListsId) if BLACKLIST_EXTRA_SOCKETS.include? defaultBonusListsId # extra sockets
-        context_data['bonusSummary']['defaultBonusLists'].delete(defaultBonusListsId) if BLACKLIST_TERTIARY_STATS.include? defaultBonusListsId # avoidance, leech, speed, indestructible
-        context_data['bonusSummary']['defaultBonusLists'].delete(defaultBonusListsId) if BLACKLIST_RANDOM_SUFFIXES.include? defaultBonusListsId # random suffixes
+        context_data['bonusSummary']['defaultBonusLists'].delete(defaultBonusListsId) unless BONUS_ID_WHITELIST.include? defaultBonusListsId
       end
+
+      # if the context is trade-skill, add these bonus IDs manually since they don't
+      # aren't in the data returned from Blizzard for whatever reason.
+      if context == 'trade-skill'
+        context_data['bonusSummary']['defaultBonusLists'] =
+          context_data['bonusSummary']['defaultBonusLists'] + TRADESKILL_BONUS_IDS
+      end
+
       if context_data['bonusSummary']['defaultBonusLists'].empty?
         context_data['bonusSummary']['defaultBonusLists'] = [0]
       end
+
       context_data['bonusSummary']['defaultBonusLists'].each do |defaultBonus|
         options = {
             :remote_id => id,
@@ -321,9 +319,7 @@ class Item
           # if available we need to import warforged too
           # the other stuff like extra socket or tertiary stats are added in the UI dynamically
           context_data['bonusSummary']['chanceBonusLists'].each do |bonus|
-            next if BLACKLIST_EXTRA_SOCKETS.include? bonus # avoidance, leech, speed, indestructible
-            next if BLACKLIST_RANDOM_SUFFIXES.include? bonus # random suffix
-            next if BLACKLIST_TERTIARY_STATS.include? bonus # tertiary stats
+            next unless BONUS_ID_WHITELIST.include? bonus
             puts bonus
             options = {
                 :remote_id => id,
